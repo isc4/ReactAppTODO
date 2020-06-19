6f866e91627340e5e07efd62e2475ab5de06c1bd
Ignat <ignat.chegodaykin@gmail.com>
2020-06-19 17:03:53 +0500

    Первый этап создания todo приложения

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ListIcon from './assets/img/listicon.svg';
+import PensilIcon from './assets/img/pensil.svg';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="board">
+          <div className="board__side-bar">
+            <List items={[
+              {
+                icon: <img src={ListIcon} alt="icon tasks list"/>,
+                name: 'Все задачи'
+              
+              }
+            ]}/>
+                        <List items={[
+              {
+                color: 'green',
+                name: 'Покупки'
+              },
+              {
+                color: 'blue',
+                name: 'Фронтенд',
+                active: true
+              },
+              {
+                color: 'pink',
+                name: 'Фильмы и сериалы'
+              },
+              {
+                color: 'teal',
+                name: 'Книги'
+              },
+              {
+                color: 'grey',
+                name: 'Личное'
+              },
+            ]}/>
+          </div>
+          <div className="board__tasks">
+            <div className="board__header">
+              <h1>Фронтенд</h1>
+              <span><img src={PensilIcon} alt=""/></span>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
